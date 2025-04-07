4bfc99a12e2b28b17fdc81d47957d4451b428923
Caodongying <jsjhcdy@163.com>
2025-04-07 23:27:50 +0200

    继续搭建项目结构,添加get-short-url post端口的demo

--- app/generate_short_url.go
+++ app/generate_short_url.go
@@ -1,2 +1,27 @@
 // 用于为长链生成短链
 package app
+
+import(
+	"github.com/gin-gonic/gin"
+)
+
+type getShortUrlRequest struct {
+	Url string  `json:"url" binding:"required"` // 如果Url不大写就无法被ShouldBind()访问到
+	// 需要json标签是表明结构体中的Url是和JSON数据中的url绑定，注意两者大小写不一样，不写标签就无法完全匹配
+}
+
+func getShortUrlHandler(context *gin.Context){
+	var request getShortUrlRequest
+	if err := context.ShouldBind(&request); err != nil {
+		panic("参数有误")
+	}
+	shortUrl := generateShortUrl(request.Url)
+	// 写入response
+	context.JSON(200, gin.H{
+		"shorturl": shortUrl,
+	})
+}
+
+func generateShortUrl(longUrl string) string{
+	return longUrl
+}
--- app/router.go
+++ app/router.go
@@ -3,3 +3,18 @@
 */
 package app
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
+
+func StartServer(){
+	// 1. 创建路由
+	router := gin.Default()
+
+	// 2. 绑定路由规则
+	router.POST("/get-short-url", getShortUrlHandler) // gin.HandlerFunc函数不可以有返回值
+
+	// 3. 启动路由
+	router.Run(":1234")
+}
