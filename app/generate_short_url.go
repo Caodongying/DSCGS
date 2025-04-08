bfb47ed0566b6ebf7c3767bb7ef6356ed0805cb1
Caodongying <jsjhcdy@163.com>
2025-04-09 01:17:49 +0200

    引入Redis层,用于短链生成

--- app/generate_short_url.go
+++ app/generate_short_url.go
@@ -1,9 +1,16 @@
 // 用于为长链生成短链
 package app
 
-import(
+import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"strconv"
 )
+
+const RedisServer string = "localhost:6379"
+const ShortUrlServer string = "localhost:1234"
 
 type getShortUrlRequest struct {
 	Url string  `json:"url" binding:"required"` // 如果Url不大写就无法被ShouldBind()访问到
@@ -16,6 +23,11 @@ func getShortUrlHandler(context *gin.Context){
 		panic("参数有误")
 	}
 	shortUrl := generateShortUrl(request.Url)
+
+	// TODO: 将(长链，短链)对，写入Redis并进行持久化保存
+	// TODO: 允许自行设置短链有效期
+	// TODO: 设置短链服务器的域名，开发期间暂时使用localhost （应该使用配置文件进行指定）
+
 	// 写入response
 	context.JSON(200, gin.H{
 		"shorturl": shortUrl,
@@ -25,8 +37,22 @@ func getShortUrlHandler(context *gin.Context){
 /*
 核心模块：短链生成
 
-
+生成方法：获取Redis的自增ID，在末尾添加时间戳，然后进行Base62编码
 */
 func generateShortUrl(longUrl string) string{
-	return longUrl
+	// 创建Redis客户端
+	context := context.Background()
+	redisDb := redis.NewClient(&redis.Options{
+		Addr: RedisServer,
+		Password: "", //暂时还没有设置密码
+		DB: 0, //使用默认DB
+	})
+
+	fmt.Println("已经连接到Redis")
+
+	// 获取自增ID
+	id := redisDb.Incr(context, "id").Val()
+
+	// TODO: 转为Base62
+	return strconv.FormatInt(id, 10)
 }
